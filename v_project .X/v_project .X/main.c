65e8b82815a7b0f35d9760d306b7777242296070
Sebastian Rodriguez <A01700378@itesm.mx>
2022-12-04 00:46:44 -0500

    add more code comments
    Co-Authored-By: Paola <67294757+pgomezr0@users.noreply.github.com>
    Co-Authored-By: JCCalvi <48540824+JCCalvi@users.noreply.github.com>

--- v_project .X/v_project .X/main.c
+++ v_project .X/v_project .X/main.c
@@ -16,80 +16,80 @@ uint16_t ADC_READ(uint8_t channel);
 uint8_t led[12];
 
 void main(){
+    // Define variables
     uint8_t volt;
     uint8_t data ;
     
  
 //----------------------------------------------------------------------------//
 // I/O PORTS CONFIGURATION
- TRISCbits.TRISC0 = 0;
- TRISCbits.TRISC1 = 0;
- TRISCbits.TRISC2 = 0;
- TRISCbits.TRISC3 = 0;
- TRISCbits.TRISC4 = 0;
- TRISCbits.TRISC5 = 0;
- TRISCbits.TRISC6 = 0; 
- TRISCbits.TRISC7 = 0;
- TRISDbits.TRISD0 = 0;
- TRISDbits.TRISD1 = 0;
- TRISDbits.TRISD2 = 0;
- TRISEbits.TRISE0 = 0;
+ // Here we define the I/O port configuration for the 12 LEDs as OUTPUT
+    TRISCbits.TRISC0 = 0;
+    TRISCbits.TRISC1 = 0;
+    TRISCbits.TRISC2 = 0;
+    TRISCbits.TRISC3 = 0;
+    TRISCbits.TRISC4 = 0;
+    TRISCbits.TRISC5 = 0;
+    TRISCbits.TRISC6 = 0; 
+    TRISCbits.TRISC7 = 0;
+    TRISDbits.TRISD0 = 0;
+    TRISDbits.TRISD1 = 0;
+    TRISDbits.TRISD2 = 0;
+    TRISEbits.TRISE0 = 0;
 //----------------------------------------------------------------------------//
 // ADC INPUT FROM ANALOG TEMPERATURE SENSOR
 
 // OUTPUT OF ADC
 
 // UART TX AND TX PORTS
-    U1CON0bits.TXEN = 1; // Tx transimet 
-    U1CON0bits.RXEN = 1; // Rx recive 
+    U1CON0bits.TXEN = 1; // TX Transmit port 
+    U1CON0bits.RXEN = 1; // RX Recieve port (although not used for this code) 
 //----------------------------------------------------------------------------//
 // ADC CONFIGURATION
- 
-    ANSELAbits.ANSELA1 = 1; // analog  RA0
+    ANSELAbits.ANSELA1 = 1; // I/O Analog port is RA0
     ADCON2bits.MD = 0; // Mode ADC
     ADCON0bits.FM = 1; // 4 bits ADRESH MSB - ADRESL LSB 8 bits
  
 //----------------------------------------------------------------------------//
 // CHANNEL SELECTION
-   TRISAbits.TRISA0 = 1; // select  RA0
-    ANSELAbits.ANSELA0 = 1; // channel RA0
+    TRISAbits.TRISA0 = 1; // Select  RA0
+    ANSELAbits.ANSELA0 = 1; // Channel RA0
 // ADC VOLTAGE REFERENCE SELECTION
-   ADREFbits.NREF = 0; // VSS
-    ADREFbits.PREF = 0; // VDD
+    ADREFbits.NREF = 0; // VSS - Negative Voltage Reference
+    ADREFbits.PREF = 0; // VDD - Positive Voltage Reference
 // ADC CONVERSION CLOCK SOURCE
-  ADCON0bits.ON = 1; // ADC ON
+    ADCON0bits.ON = 1; // Turn ADC ON
 // INTERRUPT CONTROL
 
 // RESULT FORMATING
-   ADCON0bits.CS = 0; // Clock Fosc
+    ADCON0bits.CS = 0; // Clock Fosc
 // CONVERSION TRIGGER SELECTION
-   ADCON0bits.GO = 1; //   CONVERSION TRIGGER 
+    ADCON0bits.GO = 1; // Trigger conversion
 // ADC ADQUISITION TIME
-ADCLK = 0b001111; // 500ns = 0.5us -> TAD
+    ADCLK = 0b001111; // 500ns = 0.5us -> TAD
 // ADC PRECHARGE TIME
 
 // TURN ON ADC MODULE
-  TRISAbits.TRISA1 = 1; //  RA0
+    TRISAbits.TRISA1 = 1; // Turn ADC module ON
 // START ADC CONVERSION
 
 //----------------------------------------------------------------------------//
 // UART CONFIGURATION
- 
     U1RXPPS = 0x29; // RF1 -> RX 
     //U1TXB = 'A'; //->> TSR
 //----------------------------------------------------------------------------//
 // PIN MAPPING
-  RF0PPS = 0x20; // RF0 -> TX
+    RF0PPS = 0x20; // RF0 -> TX
 // TRANSMITTER ENABLING
-  U1CON0bits.BRGS = 0; // Formula 16
-    U1BRG = 416; // 9600 buad 
+    U1CON0bits.BRGS = 0; // Formula 16
+    U1BRG = 416; // 9600 Baud rate
     U1CON0bits.MODE = 0; // Mode 8 bits UART
     U1CON1bits.ON = 1;   
-    TRISFbits.TRISF1 = 1; // enable
+    TRISFbits.TRISF1 = 1; // Enable
     ANSELFbits.ANSELF1 = 0; // Digital
-    WPUFbits.WPUF1 = 0; // pins pull up
+    WPUFbits.WPUF1 = 0; // Pins Pull Up
     INLVLFbits.INLVLF1 = 0; // TTL
-    SLRCONFbits.SLRF1 = 1; // limit
+    SLRCONFbits.SLRF1 = 1; // Limit
     ODCONFbits.ODCF1 = 0; // Push Pull 
 //----------------------------------------------------------------------------//
 // INTERRUPTS CONFIGURATION
@@ -98,8 +98,13 @@ ADCLK = 0b001111; // 500ns = 0.5us -> TAD
 
 while(1){
  uint8_t adc =ADC_READ(1);
+ //Conversion to VOLTS
 volt = adc*0.0008056; 
+
+//Send volts over UART
 UART_Tx(volt);
+
+//Turn LEDs proportional to sensor voltage
 for(int i=0;i<12;i++){
 led[12]=adc;
    LATCbits.LATC0 = led[0]; // LED 0
@@ -108,12 +113,12 @@ led[12]=adc;
    LATCbits.LATC3 = led[3];
    LATCbits.LATC4 = led[4];
    LATCbits.LATC5 = led[5];
-   LATCbits.LATC6 = led[6]; //
+   LATCbits.LATC6 = led[6]; 
    LATCbits.LATC7 = led[7];
    LATBbits.LATB5 = led[8];
    LATDbits.LATD1 = led[9];
    LATDbits.LATD2 = led[10];
-   LATEbits.LATE0 = led[11];
+   LATEbits.LATE0 = led[11]; // LED 12
 }
 };
 
